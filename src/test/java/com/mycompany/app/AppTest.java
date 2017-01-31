2bbee429e9a82692a4e1fc93af5223ca1f35ec49
Napcazz <burakdurgunsu@hotmail.com>
2017-01-31 16:06:43 +0300

    First commit.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,6 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Unit test for simple App.
  */
@@ -34,5 +37,24 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+	
+    public void testFound() {
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      assertTrue(new App().search(array, 4));
+    }
+
+    public void testNotFound() {
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      assertFalse(new App().search(array, 5));
+    }
+
+    public void testEmptyArray() {
+      ArrayList<Integer> array = new ArrayList<>();
+      assertFalse(new App().search(array, 1));
+    }
+
+    public void testNull() {
+      assertFalse(new App().search(null, 1));
     }
 }
